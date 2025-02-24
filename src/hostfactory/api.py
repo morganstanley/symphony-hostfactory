a0eef514f7cc01a411aec72eba204fe9fdc74061
cyriaquem <cyriaque.millot@morganstanley.com>
2025-02-24 13:45:52 +0000

    Adding headers
    Making sure we are compliant with the contribution conditions.

--- src/hostfactory/api.py
+++ src/hostfactory/api.py
@@ -1,4 +1,8 @@
-"""Low level hostfactory API."""
+"""
+Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License"). You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+
+Low level hostfactory API."""
 
 import json
 import logging
@@ -85,7 +89,7 @@ def request_machines(workdir, count):
 
     # The request id is generated, directory should not exist.
     #
-    # TODO(andreik): handle error if directory already exists.
+    # TODO: handle error if directory already exists.
     dst_path = pathlib.Path(workdir) / "requests" / request_id
     tmp_path = _mktempdir(workdir)
 
@@ -218,7 +222,7 @@ def request_return_machines(workdir, machines):
     """Request to return machines based on the provided hostfactory input JSON."""
     # pylint: disable=too-many-locals
     #
-    # TODO(andreik): duplicate code, create a function.
+    # TODO: duplicate code, create a function.
     hf_pods_dir = pathlib.Path(workdir) / "pods"
     hf_return_reqs_dir = pathlib.Path(workdir) / "return-requests"
 
--- src/hostfactory/cli/hf.py
+++ src/hostfactory/cli/hf.py
@@ -1,4 +1,9 @@
-"""Implements the Symphony HostFactory provider required interfaces."""
+"""
+Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License"). You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+
+Implements the Symphony HostFactory provider required interfaces.
+"""
 
 import json
 import logging
@@ -96,7 +101,7 @@ def request_machines(ctx, json_file) -> None:
     request = json.loads(file_content)
     logging.info("request_machines: %s", request)  # noqa: TID251
 
-    # TODO(andreik): handle input validation
+    # TODO: handle input validation
     count = request["template"]["machineCount"]
 
     response = hfapi.request_machines(workdir, count)
