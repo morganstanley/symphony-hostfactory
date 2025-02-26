35e4fc2bd8df311cd4bf403882ef79801844c22e
cyriaquem <cyriaque.millot@morganstanley.com>
2025-02-26 10:56:54 +0000

    Pass ruff check
    Enable ruff check on project

--- src/hostfactory/tests/regression/conftest.py
+++ src/hostfactory/tests/regression/conftest.py
@@ -1,6 +1,11 @@
-"""
-Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License"). You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. See the NOTICE file distributed with this work for additional information regarding copyright ownership.
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+"""Morgan Stanley makes this available to you under the Apache License, Version 2.0
+(the "License"). You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0. See the NOTICE file distributed
+with this work for additional information regarding copyright ownership.
+Unless required by applicable law or agreed to in writing, software distributed
+ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations under the License.
 
 Test configuration for regression testing
 """
@@ -160,7 +165,7 @@ def run_event_command() -> click.testing.Result:
     )
 
 
-def run_custom_hostfactory_test(
+def run_custom_hostfactory_test( # noaq: C901, PLR0912
     test_spec: dict,
     run_hostfactory_pods,  # pylint: disable=unused-argument  # noqa: ARG001
     run_hostfactory_machines,  # noqa: ARG001
